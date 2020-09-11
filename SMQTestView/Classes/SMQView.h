c83c68fb812ff37fba62528ae634d6a5f8e43116
孙梦青 <sunmengqing@wshifu.com>
2020-09-11 18:38:40 +0800

    init commit of SMQTestView

---
+++ SMQTestView/Classes/SMQView.h
@@ -0,0 +1,17 @@
+//
+//  SMQView.h
+//  SMQTestView_Example
+//
+//  Created by xiaoyi on 2020/9/11.
+//  Copyright © 2020 kcjm1019. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SMQView : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SMQTestView/Classes/SMQView.m
@@ -0,0 +1,13 @@
+//
+//  SMQView.m
+//  SMQTestView_Example
+//
+//  Created by xiaoyi on 2020/9/11.
+//  Copyright © 2020 kcjm1019. All rights reserved.
+//
+
+#import "SMQView.h"
+
+@implementation SMQView
+
+@end
